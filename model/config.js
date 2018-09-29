98111b6b2da8f113381c20a49c6d556b263a4f71
cangyu1993 <1765244720@qq.com>
2018-09-29 08:21:35 +0800

    服务初写

---
+++ model/config.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/note',{ useNewUrlParser: true });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('we are connected!')
+});
+
+module.exports = db;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
